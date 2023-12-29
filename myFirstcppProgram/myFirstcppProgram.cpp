3989b7ad2f5a8cca6bb1127956b6960b1785cef5
masonsquier <squier.mason@gmail.com>
2023-12-29 06:08:29 -0500

    Fixed main cpp file not having corrected source

--- myFirstcppProgram/myFirstcppProgram.cpp
+++ myFirstcppProgram/myFirstcppProgram.cpp
@@ -1,12 +1,16 @@
 #include <iostream> 
+
 #include "myHeader.h"
+#include "MyotherHeader.h"
 
 int main()
 {
 
     std::cout << "Hello World!\n";
     
-    std::cout << returnNum();
+    std::cout << returnNum() << std::endl;
+
+    std::cout << wowSus();
     
 }
 
